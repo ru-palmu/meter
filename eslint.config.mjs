90a598b090b46e88398992fa88ffd29416f2c769
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-08-23 17:26:17 +0900

    CI: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+
+import globals from "globals";
+
+// eslint.config.mjs
+export default [
+  {
+    files: ["**/*.js"], // 全ての JS を対象
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+	  globals: {
+		...globals.browser,
+		...globals.node,
+		...globals.es2021,
+
+		// data
+		PALMU_NOTICES: "readonly",
+		EVENTS_LOCAL: "readonly",
+
+		// meter.js
+		presets: "readonly",
+		default_rank: "readonly",
+
+		// plan.js
+		calculatePlans: "readonly",
+		loadDefaultPlan: "readonly",
+
+		// coin.js
+		copyResult: "readonly",
+
+		// Chart.js
+		Chart: "readonly",
+
+		// navi.js
+		cand_rank: "readonly",
+		loadDefaultValues: "readonly",
+		formatAsK: "readonly",
+		formatPalmu: "readonly",
+		hashChangeGlossary: "readonly",
+		latestDate: "readonly",
+		renderGlossary: "readonly",
+        renderNavis: "readonly",
+		saveSessionArgs: "readonly",
+		selectedRank: "readonly",
+		setRankText: "readonly",
+		setupTooltips: "readonly",
+		score2coin: "readonly",
+	  },
+    },
+    rules: {
+      // 文法・バグ検出中心のルール
+      "no-unused-vars": ["warn", {
+        "argsIgnorePattern": "^__|^_$",
+        "varsIgnorePattern": "^__|^_$",
+	  }],
+      "no-undef": "error",
+      "no-console": "off",
+      "no-debugger": "error",
+      "no-dupe-keys": "error",
+      "no-redeclare": "error",
+      "no-fallthrough": "warn",
+      // 必要に応じて追加
+    },
+  },
+];
